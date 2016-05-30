e0758db48c28fa267fc88dc81e95815cba99d1d7
Nolan Frazier <nofrazier3@gmail.com>
2016-05-29 20:22:10 -0700

    Added log in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root "users#new"
   get 'signup' => 'users#new'
   resources :users
+  get '/login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 end
